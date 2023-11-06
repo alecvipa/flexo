a47c4d434935766fa788e173844bba5e52ae71e1
alecvipa <hebrit_626@hotmail.com>
2023-11-06 15:27:18 -0500

    added new

--- server.js
+++ server.js
@@ -66,9 +66,9 @@ app.post("/ajax/email", function (request, response) {
 
     var htmlBody = `<h2>Correo de contacto</h2><p>Nombre: ${request.body.name} </p> <p>Número de contacto: ${request.body.number} </p> <p> Correo electrónico: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Mensaje: ${request.body.message} </p>`;
     var mail = {
-        from: '"Team: Xyncs Web Studio',
-        to: 'hebrit_626@hotmail.com',
-        subject: '¡Nuevo Mensaje de SMART KIDS!',
+        from: '"Team: FLEXO Consultora',
+        to: 'contacto@consultoraflexo.com',
+        subject: '¡Nuevo Mensaje en FLEXO!',
         html: htmlBody
     };
     transporter.sendMail(mail, function (err, info) {
